ea837f9ab9234a1097d4aa4df0105fe6406f6297
sdp-srh <105343612+sdp-srh@users.noreply.github.com>
2022-05-22 22:40:07 +0200

    filesystem reader
    recursive example

---
+++ Examples/src/algorithms/FileSystemWalker.java
@@ -0,0 +1,39 @@
+package algorithms;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class FileSystemWalker {
+
+	ArrayList<String> entries = new ArrayList<String>();
+	
+	public FileSystemWalker() {
+		entries = new ArrayList<String>();
+	}
+	
+	public static void main(String args[]) {
+		FileSystemWalker fsw = new FileSystemWalker();
+		fsw.readFolder(new File("C:\\srh\\github"));
+		fsw.printEntries();
+	}
+	
+	public void readFolder(File folder) {
+		File[] files = folder.listFiles();
+		for (File f:files) {
+			// we add each entry
+			this.entries.add(f.getAbsolutePath());
+			if (f.isDirectory()) {
+				// read the sub folder content
+				this.readFolder(f);
+			}
+		}
+	}
+	
+	public void printEntries() {
+		for (int i=0;i<this.entries.size();i++) {
+			System.out.println(this.entries.get(i));
+		}
+	}
+	
+	
+}
